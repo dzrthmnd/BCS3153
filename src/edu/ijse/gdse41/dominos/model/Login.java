0bd779b9d5f363b46404aef6dacca312a71a6c6a
dzarith <47629903+dzrthmnd@users.noreply.github.com>
2019-12-03 12:23:36 +0800

    i add email

--- src/edu/ijse/gdse41/dominos/model/Login.java
+++ src/edu/ijse/gdse41/dominos/model/Login.java
@@ -7,12 +7,14 @@ package edu.ijse.gdse41.dominos.model;
 public class Login {
     private String userName;
     private String password;
+    private String email;
     
     public Login(){}
     
-    public Login(String userName,String password){
+    public Login(String userName,String password, String email){
         this.userName=userName;
         this.password=password;
+         this.email=email;
     }
     public String getUserName() {
         return userName;
